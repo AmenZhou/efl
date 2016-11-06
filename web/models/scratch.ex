a1949b2344be65e04acf5dcae52a63c44b12ed51
hzhou <hzhou@teladoc.com>
2016-11-06 12:24:42 -0500

    parse html

--- web/models/scratch.ex
+++ web/models/scratch.ex
@@ -1,4 +1,15 @@
 defmodule ClassificationUtility.Scratch do
+  def parse_items do
+    Enum.each(items, &parse_item/1)
+  end
+
+  def parse_item(item) do
+    IO.puts "Title: "
+    get_title(item) |> IO.puts
+    IO.puts "Link: "
+    get_link(item) |> IO.puts
+  end
+
   def html do
     case HTTPotion.get(url) do
       %{ body: body } ->
@@ -16,6 +27,21 @@ defmodule ClassificationUtility.Scratch do
     case html do
       { :ok, html_body } ->
         html_body |> Floki.parse |> Floki.find(".bg_small_yellow")
+      { :error, message } ->
+        message
     end
+  end
+
+  def get_title(item) do
+    item
+    |> Floki.find(".topictitle a")
+    |> Floki.text
+    |> String.strip
+  end
+
+  def get_link(item) do
+    item
+    |> Floki.find(".topictitle a")
+    |> Floki.attribute("href")
   end
 end
