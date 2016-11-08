84faf9989c35db59c59f5a7ae53643f963489edb
hzhou <hzhou@teladoc.com>
2016-11-07 20:30:26 -0500

    define schema in Dadi

--- web/models/dadi.ex
+++ web/models/dadi.ex
@@ -1,5 +1,15 @@
 defmodule ClassificationUtility.Dadi do
-  use Ecto.Schema
+  use ClassificationUtility.Web, :model
+
+  schema "dadi" do
+    field :title
+    field :url
+    field :content, :string
+    field :post_date, Ecto.DateTime
+    timestamps()
+  end
+  alias ClassificationUtility.Dadi
+
   @base_url "http://c.dadi360.com"
 
   def post_list do
