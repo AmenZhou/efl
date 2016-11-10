9bb5b44d9970d15f35ebe9e295470e967e4ed4fa
hzhou <hzhou@teladoc.com>
2016-11-09 19:41:52 -0500

    insert title and url to db

--- web/models/dadi.ex
+++ web/models/dadi.ex
@@ -4,19 +4,40 @@ defmodule ClassificationUtility.Dadi do
   schema "dadi" do
     field :title
     field :url
-    field :content, :string
+    field :content
     field :post_date, Ecto.DateTime
     timestamps()
   end
   alias ClassificationUtility.Dadi
+  alias ClassificationUtility.DadiCategory
+  alias ClassificationUtility.Repo
 
   @base_url "http://c.dadi360.com"
 
   def post_list do
-    ClassificationUtility.DadiCategory.parse_items(url)
+    DadiCategory.parse_items(url)
+    |> Enum.each(&insert/1)
+  end
+
+  def insert(item \\ %{}) do
+    set = changeset(%Dadi{}, item)
+    #IO.inspect set
+
+    case Repo.insert(set) do
+      { :ok, _ } ->
+        IO.puts("Insert one item")
+      { :error, _ } ->
+        IO.puts("Error")
+    end
   end
 
   def url do
     @base_url <> "/c/forums/show/53.page"
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title, :url, :content, :post_date])
+    |> validate_required([:title, :url])
   end
 end
