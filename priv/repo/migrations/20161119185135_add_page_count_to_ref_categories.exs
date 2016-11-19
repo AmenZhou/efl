c20ea35e93f113deaf47006cbe6329f205aef36a
hzhou <hzhou@teladoc.com>
2016-11-19 13:55:24 -0500

    add new column page size into categories

---
+++ priv/repo/migrations/20161119185135_add_page_count_to_ref_categories.exs
@@ -0,0 +1,15 @@
+defmodule ClassificationUtility.Repo.Migrations.AddPageCountToRefCategories do
+  use Ecto.Migration
+
+  def up do
+    alter table(:ref_categories) do
+      add :page_size, :integer
+    end
+  end
+
+  def down do
+    alter table(:ref_categories) do
+      remove :page_size
+    end
+  end
+end
