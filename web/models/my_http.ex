814970840006e88b627f254e761d9d02fd49b14c
amenzhou <chou.amen@gmail.com>
2020-04-18 20:12:50 -0400

    rewrite the fetch_proxy

--- web/models/my_http.ex
+++ web/models/my_http.ex
@@ -2,14 +2,11 @@ defmodule Efl.MyHttp do
   require IEx
   require Logger
   alias Efl.Proxy
-  @timeout 120_000
+  @timeout 4_000
+  @max_attempt 12
 
-  def request(url, attempts \\ 1) do
-    if attempts >= 12 do
-      Proxy.fetch_proxy(true)
-      raise("Has reached the max attempts of fetching category page, #{url}")
-    end
-
+  def request(url, attempts \\ 1)
+  def request(url, attempts) when attempts < @max_attempt do
     # Fetch proxy after 3 retries
     proxy = case Integer.mod(attempts, 3) do 
       0 ->
@@ -33,6 +30,11 @@ defmodule Efl.MyHttp do
         Logger.info("Retry... #{attempts+1} attempts")
         request(url, attempts + 1)
     end
+  end
+
+  def request(url, attempts) when attempts >= @max_attempt do
+    Proxy.fetch_proxy(true)
+    raise("Has reached the max attempts of fetching category page, #{url}")
   end
 
   def proxy_config(proxy) do
